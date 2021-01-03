95e27c0be9c41116d102a2b937618b30d193c98e
Pedram Valiani <p.valiani@gmail.com>
2021-01-03 00:28:55 +0000

    fetches dogs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,31 @@ function App() {
   // Create Dog state variable
   const [dogs, setDogs] = useState(null);
 
+  // Fetch the dogs
 
+  const fetchDogs = () => {
+
+    const url = 'https://dog.ceo/api/breeds/image/random'
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => {
+
+      setDogs(jsonData);
+
+    })
+
+    
+
+
+  };
+
+
+  // The useEffect is actually performing the fetchDogs method and returning it to the state of dogs
+  
+  useEffect(() => {
+    fetchDogs();
+  }, []);
 
 
   return (
